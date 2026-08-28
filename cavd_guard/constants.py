ec1bc47b3777e2bad4f3eb9565e2dea093cf4409
serendipity098 <103930726+serendipity098@users.noreply.github.com>
2026-08-28 13:04:26 +0800

    Use mechanism-oriented recovery names in constants.py

--- cavd_guard/constants.py
+++ cavd_guard/constants.py
@@ -4,7 +4,7 @@ import re
 
 
 ZERO_WIDTH_PATTERN = re.compile(r"[\u200b-\u200f\u202a-\u202e\u2060-\u206f\ufeff]")
-EMOJI_PATTERN = re.compile(
+PICTOGRAPHIC_SYMBOL_PATTERN = re.compile(
     r"[\U0001F000-\U0001FFFF"
     r"\U00002700-\U000027BF"
     r"\U00002600-\U000026FF"
@@ -13,7 +13,7 @@ EMOJI_PATTERN = re.compile(
     r"\U000E0020-\U000E007F"
     r"\ufe0e\ufe0f\u20e3]"
 )
-MASK_TOKEN_PATTERN = re.compile(
+PLACEHOLDER_TOKEN_PATTERN = re.compile(
     r"(\[\s*M\s*[\W_]*A\s*[\W_]*S\s*[\W_]*K\s*(\d*)\s*\]"
     r"|\{\s*M\s*[\W_]*A\s*[\W_]*S\s*[\W_]*K\s*(\d*)\s*\}"
     r"|<\s*M\s*[\W_]*A\s*[\W_]*S\s*[\W_]*K\s*(\d*)\s*>"
@@ -21,20 +21,21 @@ MASK_TOKEN_PATTERN = re.compile(
     r"|\bM\s*[\W_]*A\s*[\W_]*S\s*[\W_]*K\s*(\d*)\b)",
     flags=re.IGNORECASE,
 )
-ASCII_CONFUSABLE_TRANSLATION = str.maketrans({"0": "o", "1": "l", "3": "e", "4": "a", "5": "s", "7": "t", "@": "a", "$": "s"})
-ANCHOR_TRANSLATION = str.maketrans({"$": "S", "3": "e", "7": "t", "@": "a", "0": "o"})
-SINGLE_LETTER_RUN_PATTERN = re.compile(r"\b(?:[A-Za-z]\s+){2,}[A-Za-z]\b")
+ASCII_CONFUSABLE_NORMALIZATION = str.maketrans({"0": "o", "1": "l", "3": "e", "4": "a", "5": "s", "7": "t", "@": "a", "$": "s"})
+ANCHOR_CONFUSABLE_NORMALIZATION = str.maketrans({"$": "S", "3": "e", "7": "t", "@": "a", "0": "o"})
+FRAGMENTED_LETTER_SEQUENCE_PATTERN = re.compile(r"\b(?:[A-Za-z]\s+){2,}[A-Za-z]\b")
 WORD_PATTERN = re.compile(r"\b[A-Za-z][A-Za-z0-9@$]{2,24}\b")
-UBBI_QUESTION_RE = re.compile(
+RECURRING_AFFIX_QUOTED_PAYLOAD_PATTERN = re.compile(
     r"(?i)(?:my\s+)?question\s+is\s*[\"'\u201c\u201d](.*?)[\"'\u201c\u201d]\s*,\s*which is in ubbi dubbi",
     re.DOTALL,
 )
-UBBI_QUESTION_FALLBACK_RE = re.compile(
+QUOTED_PAYLOAD_FALLBACK_PATTERN = re.compile(
     r"(?i)(?:my\s+)?(?:question|request|query|prompt|instruction)\s+is\s*[\"'\u201c\u201d](.*?)[\"'\u201c\u201d]",
     re.DOTALL,
 )
-UBBI_VOWEL_PREFIX_RE = re.compile(r"(?i)ub(?=[aeiouAEIOU])")
-RISKY_CANONICAL_TERMS = {
+RECURRING_AFFIX_MARKER_PATTERN = re.compile(r"(?i)ub")
+VOWEL_PREFIX_FRAGMENT_PATTERN = re.compile(r"(?i)ub(?=[aeiouAEIOU])")
+BOUNDED_REPAIR_VOCABULARY = {
     "attack",
     "backdoor",
     "bomb",
